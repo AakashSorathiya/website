9fbd3dbf5aac85544c5adaafe723674d148ec01e
Jeremy Pitzeruse <jpitzeruse@gmail.com>
2013-05-14 09:37:38 -0400

    Updated the ignore file to forget about the DS_Store files that mac creates.

--- application/config/database.php
+++ application/config/database.php
@@ -54,7 +54,7 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
-$db['default']['password'] = 'root';
+$db['default']['password'] = '';
 $db['default']['database'] = 'w_seo';
 
 // $db['default']['hostname'] = getConfigValue('dbHost_w_seo');
