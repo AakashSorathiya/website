08829ee99bd66d6ccd05dd60c0107adcf9c63494
Ryan Hoffmann <rhoff37@gmail.com>
2015-10-13 09:52:27 -0400

    Updated Announcements

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -47,7 +47,7 @@
 <h2><b>New Student Employees</b></h2>
 <p>Welcome to RIT and the first steps of obtaining on campus employment!  Below is a brief overview of what you should know and do for your first quarter at RIT.</p>
 <ul>
-	<li><p>Make sure you are registered for full-time status for the semester.</p></li>
+	<li><p>Make sure you are registered for at least 12 credit hours this quarter.</p></li>
 	<li><p>Read through the <a href="?/home/job/">Getting a Job</a> page to learn about how to go about finding a job on campus.</p></li>
 </ul>
 
@@ -55,7 +55,7 @@
 <p>Welcome back to RIT and good luck to you this quarter!</p>
 <ul>
 	<li><p>Don't forget to renew your SEO Employment Eligibility Card at our office!</p></li>
-	<li><p>Make sure you are registered for at least 12 credit hours this quarter.</p></li>
+	<li><p>Make sure you are registered for full-time status for the semester.</p></li>
 	<li><p>If you are working on your thesis or project, bring us a copy of the Full-Time Equivalency Form.</p></li>
 	<li><p>All W-2, W-4, and W-5 forms as well as New York State IT-2104 and IT-2104-E forms need to be sent to Payroll in the Eastman Building on the first floor.</p></li>
 </ul>
